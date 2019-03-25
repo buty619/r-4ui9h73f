5abc1940d3480370b779a986fc2a54865fa197b8
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-25 17:54:52 -0500

    push

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      texto : ""
+    }
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input id="inputText" type="text" onChange = {this.upDate.bind(this)} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.texto}</p>
       </div>
     );
+  }
+  upDate(){
+    this.setState({
+      texto : document.getElementById("parrafo").value
+    });
   }
 }
 
